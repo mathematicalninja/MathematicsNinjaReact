36e636be64d7bcae37eb34d93c10829377ece73d
mathematicalninja <github@mathematics.ninja>
2021-05-04 10:32:52 +0100

    Borders work

--- src/Testing/BlogClass.js
+++ src/Testing/BlogClass.js
@@ -14,80 +14,128 @@ class BlogClass extends React.Component {
             style: {
                 "marginLeft": "2vw",
                 "marginRight": "2vw",
-                "backgroundColor": "var(--Primary-1)",
+                "backgroundColor": "var(--Primary-2)",
                 "color": "var(--Grey-0)",
                 "fontSize": "xx-Large",
                 // "border": "5px solid var(--Secondary-0)",
                 "boxShadow":
-                    "inset var(--Secondary-0) 0 0 0 5px," +
-                    "inset var(--Secondary-1) 0 0 0 6px," +
-                    "inset var(--Secondary-2) 0 0 0 10px," +
-                    "inset var(--Secondary-3) 0 0 0 11px," +
-                    "inset var(--Secondary-4) 0 0 0 16px," +
-                    "inset var(--Secondary-5) 0 0 0 17px," +
-                    "inset var(--Secondary-6) 0 0 0 22px," +
-                    "inset var(--Secondary-7) 0 0 0 23px",
+                    " var(--Secondary-0) 0 0 0 5px," +
+                    " var(--Secondary-1) 0 0 0 6px," +
+                    " var(--Secondary-2) 0 0 0 10px," +
+                    " var(--Secondary-3) 0 0 0 11px," +
+                    " var(--Secondary-4) 0 0 0 16px," +
+                    " var(--Secondary-5) 0 0 0 17px," +
+                    " var(--Secondary-6) 0 0 0 22px," +
+                    " var(--Secondary-7) 0 0 0 23px",
             },
             "internal-Style": {
+                // vars for width of outside border, and the gap between that and the text e.g. 25px = border (23px) +gap(2px)
                 marginLeft: 23,
                 marginRight: 23,
                 "margin-top": 23,
-                "margin-bottom": 230,
+                "marginBottom": 23,
             },
+
             pageTitleStyle: {
                 "textAlign": "center",
+                // "marginTop": 23,
+                // marginLeft: 23,
+                // marginRight: 23,
 
-                "-webkit-text-fill-color": "var(--Secondary-6)",
-                "-webkit-text-stroke-width": "1.5px",
-                "-webkit-text-stroke-color": "var(--Grey-0)",
+                "WebkitTextFillColor": "var(--Secondary-6)",
+                "WebkitTextStrokeWidth": "1.5px",
+                "WebkitTextStrokeColor": "var(--Grey-0)",
                 // "textShadow": "" +
                 //     "1.5px -1.5px 0px var(--Grey-5)," +
                 //     "-1.5px -1.5px 0px var(--Grey-5)",
-                "border-bottom": "3px double var(--Accent-0)"
+                "borderBottom": "3px double var(--Accent-0)"
             },
             smallTitleStyle: {
-                "-webkit-text-fill-color": "var(--Secondary-3)",
-                "-webkit-text-stroke-width": "1px",
-                "-webkit-text-stroke-color": "var(--Grey-0)",
+                "WebkitTextFillColor": "var(--Secondary-0)",
+                "WebkitTextStrokeWidth": ".5px",
+                "WebkitTextStrokeColor": "var(--Secondary-2)",
+                "marginBottom": "4px",
+                "marginTop": "23px",
             },
             mainTextStyle: {
-                "-webkit-text-fill-color": "var(--Grey-0)",
-                "-webkit-text-stroke-width": ".5px",
-                "-webkit-text-stroke-color": "var(--Grey-2)",
+                "WebkitTextFillColor": "var(--Grey-0)",
+                "WebkitTextStrokeWidth": ".5px",
+                "WebkitTextStrokeColor": "var(--Grey-2)",
                 "textAlign": "justify ",
             },
+
+
             endOfSentence: <span> &nbsp;</span>,
             endOfParagraph: <span>{"\n\n"}</span>,
             // section splits are for future updating.
-            endOfsection: <div style={{
-                "border-bottom": "2px dotted var(--Accent-0)",
+            endOfSection: <div style={{
+                "borderBottom": "2px dotted var(--Accent-0)",
+                "marginTop": "23px",
+                // dashes are not going to the edges.
+                "marginLeft": "0",
+                "marginRight": "0",
 
-            }} />
+            }} />,
+            endOfLastSection: <div style={{
+                // "borderBottom": "2px dotted var(--Accent-0)",
+                "marginTop": "23px",
+
+            }} />,
+            endOfLastSection: <div style={{
+                // "border-bottom": "2px dotted var(--Accent-0)",
+                "marginBottom": "0px",
+            }} />,
         }
     }
     sentenceSplit(paragraph) {
+        // unique keys
         let sentenceArray = [];
-        paragraph.forEach(sentence => {
-            sentenceArray.push(<span>{sentence}{this.state.endOfSentence}</span>)
-        });
+        for (let index = 0; index < paragraph.length; index++) {
+            let sentence = paragraph[index]
+            if (index == paragraph.length - 1) {
+                sentenceArray.push(<span>{sentence}</span>)
+            } else {
+                sentenceArray.push(<span>{sentence}{this.state.endOfSentence}</span>)
+            }
+        }
+        // paragraph.forEach(sentence => {
+
+        // });
         return sentenceArray
     }
     paragraphSplit(section) {
+        // unique keys
+        // add in a no-title option for sections
         let paragraphArray = [<h3 style={this.state.smallTitleStyle}>{section.Title}</h3>];
-        section.Content.forEach(paragraph => {
-            paragraphArray.push(<div>
-                {this.sentenceSplit(paragraph)}{this.state.endOfParagraph}
-            </div>)
-        });
+        for (let index = 0; index < section.Content.length; index++) {
+            let paragraph = section.Content[index]
+            if (index === section.Content.length) {
+                paragraphArray.push(<div>
+                    {this.sentenceSplit(paragraph)}{this.state.endOfParagraph}
+                </div>)
+            } else {
+                paragraphArray.push(<div>
+                    {this.sentenceSplit(paragraph)}
+                </div>)
+            }
+        }
         return paragraphArray
     }
     sectionSplit(FullText) {
+        // unique keys
         let sectionArray = [];
-        FullText.forEach(section => {
-            sectionArray.push(<div style={this.state.mainTextStyle}>{this.paragraphSplit(section)}{this.state.endOfsection}</div>)
-            // if last entry, don't Hoiz break
-            // sectionArray.push(<div>{this.paragraphSplit(section)}</div>)
-        });
+
+        for (let index = 0; index < FullText.length; index++) {
+            let section = FullText[index];
+            console.log(index, section, FullText.length, index !== FullText.length - 1, this.state.endOfLastSection)
+            if (index !== FullText.length - 1) {
+
+                sectionArray.push(<div style={this.state.mainTextStyle}>{this.paragraphSplit(section)}{this.state.endOfSection}</div>)
+            } else {
+                sectionArray.push(<div style={this.state.mainTextStyle}>{this.paragraphSplit(section)}{this.state.endOfLastSection}</div>)
+
+            }
+        }
         return sectionArray
     }
     blogStrucure(blogData) {
