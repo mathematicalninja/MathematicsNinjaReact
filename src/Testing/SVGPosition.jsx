2d8420911eb9015c57a2a55272ebea23150081f5
mathematicalninja <github@mathematics.ninja>
2021-05-13 04:16:05 +0100

    New Icon

--- src/Testing/SVGPosition.jsx
+++ src/Testing/SVGPosition.jsx
@@ -7,6 +7,7 @@ import "../../public/icon.svg"
 
 // import Disc from "./Disc.svg"
 import Disc from "./Disc.jsx"
+import Icon from "./testIcon.jsx"
 
 class SVGPostion extends React.Component {
     constructor(props) {
@@ -35,16 +36,16 @@ class SVGPostion extends React.Component {
         let gL = 81 - x
         let gW = 87 - 2 * x
 
-        // console.log("(x, y, gH, gL, gW) = ", x, y, gH, gL, gW)
-        // console.log("x", x)
-        // console.log("y", y)
-        // console.log("gH", gH)
-        // console.log("gL", gL)
-        // console.log("gW", gW)
+        console.log("(x, y, gH, gL, gW) = ", x, y, gH, gL, gW)
+        console.log("x", x)
+        console.log("y", y)
+        console.log("gH", gH)
+        console.log("gL", gL)
+        console.log("gW", gW)
 
-        // console.log("P_L", x + gL, y)
-        // console.log("P_E", x, y + gH + 180)
-        // console.log("P_Z", x + gW + 315, y + gH + 180)
+        console.log("P_L", x + gL, y)
+        console.log("P_E", x, y + gH + 180)
+        console.log("P_Z", x + gW + 315, y + gH + 180)
 
 
         let testDiv =
@@ -77,12 +78,24 @@ class SVGPostion extends React.Component {
 
         return (
             <div>
+                {/* {testDiv} */}
                 <Disc
                     outerEdge="var(--Grey-0)"
                     outerColour="var(--Vivids-5)"
                     innerEdge="var(--Grey-0)"
                     innerColour="var(--Vivids-6)"
                     size={100}
+                />
+                {/* <img src="icon 1.svg" /> */}
+                <Icon
+                    backgroundColour="var(--Primary-2)"
+                    letColour="var(--Grey-1)"
+                    heartLine="var(--Grey-1)"
+                    heartFill="var(--Accent-4)"
+                    zeroLine="var(--Grey-1)"
+                    zeroFill="var(--Accent-4)"
+                    zeroDot="var(--Grey-8)"
+                    size={512}
                 />
             </div>
         );
--- src/core/logo.js
+++ src/core/logo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/global.scss"
 import "./logo.scss"
+import Icon from "./testIcon.jsx"
 
 class Logo extends React.Component {
     constructor(props) {
@@ -31,7 +32,17 @@ class Logo extends React.Component {
                     style={{fontSize: "4vw"}}
                 >{">"}0</span>
             </div> */}
-            <img src="./icon.svg" style={{"width": "9vw"}} />
+            {/* <img src="./icon.svg" style={{"width": "9vw"}} /> */}
+            <Icon
+                backgroundColour="var(--Primary-2)"
+                letColour="var(--Grey-1)"
+                heartLine="var(--Grey-1)"
+                heartFill="var(--Accent-4)"
+                zeroLine="var(--Grey-1)"
+                zeroFill="var(--Accent-4)"
+                zeroDot="var(--Grey-8)"
+                size="9vw"
+            />
         </div>
     }
 }
