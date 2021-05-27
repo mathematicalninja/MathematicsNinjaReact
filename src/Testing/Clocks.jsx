41bcac4857d7cbdf3e7142a43673f0c02b48ad0b
mathematicalninja <github@mathematics.ninja>
2021-05-27 02:29:30 +0100

    Made a mini-clock component

--- src/Testing/Clocks.jsx
+++ src/Testing/Clocks.jsx
@@ -1,5 +1,101 @@
 import React from "react";
 
+class MiniClock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            curTime: new Date(),
+        };
+    }
+
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                curTime: new Date()
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    timeChunck(timeOrder, timeObject) {
+        let timeString = []
+        for (const integer of timeOrder) {
+            timeString.push(this.timePiece(integer, timeObject))
+            timeString.push("/")
+        }
+        timeString.pop()
+        timeString.push(<br />)
+        return timeString
+    }
+
+    timePiece(integer, timeObject) {
+        // 0 is largest time (hours), getting smaller by integer.
+        switch (integer) {
+            case 0:
+                return timeObject.getHours()
+            case 1:
+                return timeObject.getMinutes()
+            case 2:
+                return timeObject.getSeconds()
+            default:
+                break;
+        }
+    }
+    dateChunck(timeOrder, timeObject) {
+        let dateString = []
+        for (const integer of timeOrder) {
+            dateString.push(this.datePiece(integer, timeObject))
+            dateString.push("/")
+        }
+        dateString.pop()
+        dateString.push(<br />)
+        return dateString
+    }
+
+    datePiece(integer, dateObject) {
+        // 0 is largest time (years), getting smaller by integer.
+        switch (integer) {
+            case 0:
+                return dateObject.getFullYear()
+            case 1:
+                return dateObject.getMonth()
+            case 2:
+                return dateObject.getUTCDay()
+            default:
+                break;
+        }
+    }
+
+
+    render() {
+        let p = this.props
+        let style = {
+            backgroundColor: p.backgroundColor ? p.backgroundColor : "var(--Secondary-2)",
+            fontSize: p.fontSize ? p.fontSize : "100px",
+
+
+            "color": "var(--Grey-0)",
+            "paddingLeft": "10px",
+            "paddingRight": "10px",
+            "border": "solid",
+            "textAlign": "center",
+        }
+        let name = p.name ? [p.name, <br />] : ""
+        let time = this.timeChunck(p.timeOrder, p.timeObject)
+        let date = this.dateChunck(p.timeOrder, p.timeObject)
+
+        return <div style={style}>
+            {name}
+            {time}
+            {date}
+        </div>
+    }
+}
+
 class Clocks extends React.Component {
     constructor(props) {
         super(props);
@@ -80,10 +176,13 @@ class Clocks extends React.Component {
                     {T.getFullYear()}/{T.getMonth()}/{T.getUTCDay()}
                     <br />
                 </div>
-            </div >
+                <MiniClock name="heh" timeOrder={[2, 1, 0]} timeObject={this.state.curTime} />
+            </div>
         );
     }
 }
+
+
 // Sat May 15 2021 20:05:21 GMT+0100 (British Summer Time)
 
 export default Clocks
