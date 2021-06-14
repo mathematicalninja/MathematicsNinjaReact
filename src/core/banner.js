c27453d32328614dbb5832009e12f3a80e05f707
mathematicalninja <github@mathematics.ninja>
2021-06-14 08:19:02 +0100

    cleanup

--- src/core/banner.js
+++ src/core/banner.js
@@ -1,11 +1,8 @@
 import React from "react";
-import GridGame from "../Testing/GridGame";
-
 
 class Banner extends React.Component {
     constructor(props) {
         super(props)
-        // console.log("banner load")
     }
     textRender() {
         const mathsText = <span key="maths">Maths</span>
