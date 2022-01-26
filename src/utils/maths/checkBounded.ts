9265da030377c75f5a3b5a0e242b41694f0edddb
mathematicalninja <github@mathematics.ninja>
2022-01-26 05:59:51 +0000

    corrected/renamed checkBounded, made forceBounded

--- src/utils/maths/checkBounded.ts
+++ src/utils/maths/checkBounded.ts
@@ -1,9 +1,9 @@
 /**
- * if Value is between Min and Max, returns Value
+ * forces value between min and max
  *
- * otherwise returns null
+ * if min>max returns null
  */
-export function checkBounded(
+export function forceBounded(
   Min: number,
   Max: number,
   Value: number,
@@ -24,7 +24,7 @@ export function checkBoundedWithDefault(
   Value: number,
   DefaultValue: number,
 ): number {
-  const I = checkBounded(Min, Max, Value);
+  const I = forceBounded(Min, Max, Value);
   if (typeof I !== "number") {
     return DefaultValue;
   } else {
@@ -36,4 +36,11 @@ export function checkBoundedWithDefault(
  */
 function CV(N: number, X: number, V: number) {
   return (N > V ? N : V) < X ? (N > V ? N : V) : X;
+}
+
+/*
+ * checks if value is between min and max
+ */
+export function checkBounded(min: number, max: number, value: number) {
+  return min <= value && value <= max;
 }
