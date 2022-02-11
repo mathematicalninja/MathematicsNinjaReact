63f9a31582aa20b17f31958e333f7357d891cf36
mathematicalninja <github@mathematics.ninja>
2022-02-11 19:50:22 +0000

    better pause function

--- src/utils/other/pause.ts
+++ src/utils/other/pause.ts
@@ -7,4 +7,19 @@ function pause(time: number, callback?: () => void): void {
   return;
 }
 
-export default pause;
+function newPause(time: number, callback?: () => void): void {
+  // const startTime = new Date().getTime();
+  const goalTime = time + new Date().getTime();
+  // const goalTime = startTime + time;
+
+  while (true) {
+    if (new Date().getTime() >= goalTime) {
+      if (callback) {
+        callback();
+      }
+      return;
+    }
+  }
+}
+
+export default newPause;
