c2c2842616a30d03ce66e7ca7072398b1770d894
mathematicalninja <github@mathematics.ninja>
2022-02-12 02:30:50 +0000

    corrected bugs

--- src/pages/connect4.tsx
+++ src/pages/connect4.tsx
@@ -1,12 +1,9 @@
-import TicTac from "../components/games/TicTac";
-import BlogClass from "../components/blogClass/BlogClass";
-import FullPageBody from "../PageTypes/FullPageBody";
 import React from "react";
 import FcSquareGame, {
   fcSquareGameProps,
 } from "../components/games/fcSquareGame";
-import FcTicTacToe from "../components/games/fcTicTacToe";
-import { fallingCheckTile } from "../components/games/utils/handleSquareClick";
+import { yIndependentFallingCheckTile } from "../components/games/utils/handleSquareClick";
+import FullPageBody from "../PageTypes/FullPageBody";
 
 interface TTTProps extends fcSquareGameProps {
   // gridSize: [number, number];
@@ -17,13 +14,24 @@ interface TTTProps extends fcSquareGameProps {
 const FTTP: React.FC = () => {
   // function FTTP(props: { gridSize?: number[] }) {
   const TicTacToe = (
-    <FcTicTacToe
+    <FcSquareGame
+      //TODO: switch all gridSize's to being type gridSize, which is 1-indexed
       gridSize={{ x: 7, y: 6 }}
-      CheckTile={fallingCheckTile}
-      maxDiagonalLength={4}
-      minDiagonalLength={4} //FIXME: doesn't seem to pass through to the winLine calculations
-      maxHorizontalLength={7}
-      minHorizontalLength={4}
+      CheckTile={yIndependentFallingCheckTile}
+      //
+      boardStructure={{
+        diagonalType: "subLines",
+        minDiagonal: 4,
+        maxDiagonal: 6,
+
+        HorizontalType: "subLines",
+        minHorizontal: 4,
+        maxHorizontal: 7,
+
+        verticalType: "subLines",
+        minVertical: 4,
+        maxVertical: 6,
+      }}
     />
   );
 
